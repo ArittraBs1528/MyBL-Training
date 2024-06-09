7b076f391495621a7e966160b2a386d56b740a8a
Arittrabs1528 <arittra.das@brainstation-23.com>
2024-06-09 18:28:46 +0600

    done

--- Practice Day1_mybl/app/src/main/java/com/example/rechargemybl/app/adapter/ChildAdapter/PlanOfferItemViewMargin.kt
+++ Practice Day1_mybl/app/src/main/java/com/example/rechargemybl/app/adapter/ChildAdapter/PlanOfferItemViewMargin.kt
@@ -4,9 +4,9 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class PlanOfferItemViewMargin  : RecyclerView.ItemDecoration() {
+class PlanOfferItemViewMargin : RecyclerView.ItemDecoration() {
 
-    var itemDecorationCount =0;
+    var itemDecorationCount = 0;
     override fun getItemOffsets(
         outRect: Rect,
         view: View,
@@ -14,10 +14,21 @@ class PlanOfferItemViewMargin  : RecyclerView.ItemDecoration() {
         state: RecyclerView.State
     ) {
         super.getItemOffsets(outRect, view, parent, state)
-//        outRect.top = 16
-//        outRect.bottom = 10
-        outRect.left = 60
-//        outRect.right = 10
-        itemDecorationCount++
+
+
+        //current position of the element
+        val position = parent.getChildAdapterPosition(view)
+
+        val itemCount = state.itemCount
+
+        if (position == 0) {
+            outRect.left = 55
+        } else if (position == itemCount - 1) {
+            outRect.left = 13
+            outRect.right = 55
+        } else outRect.left = 13
+
+//
+
     }
 }
